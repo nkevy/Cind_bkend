d4ce0b80fcae22ca5c718e1ab98bd2615d56c506
nkevy <yvek.com@gmail.com>
2020-09-18 01:07:06 +0000

    words mth old new added

--- Call.py
+++ Call.py
@@ -1,5 +1,7 @@
 import CindAPI as cpi
 
+# todo: move code to cpi
+
 # check if a string contains forbiden char(s) 
 # return False if string can be used
 def forbiden(ss = None):
@@ -10,45 +12,111 @@ def forbiden(ss = None):
     avoid = [',','.','/','\\','[',']','+','-','=','\'','\"',';',':','?','!','@','#','$',' ','{','}']
     return any(item in ss for item in avoid)
 
+# define an error for forbiden
+# thrown by methods checking string input
+class ForbidenError(Exception):
+    pass
+
+# convert word responce into word obj
+# stop rewriteing code
+def wordobj(wrd = None):
+    if wrd is None:
+        return None
+    ret = []
+    for item in wrd:
+        ret.append(str(item))
+    return ret
+
+# format a responce list into a list of word objects
+# stop rewriting code
+def formatwordlist(wrds == None):
+    if wrds is None:
+        return None 
+    ret = []
+    for item in wrds:
+        ret.append(wordobj(item))
+    return ret
+    
 # add a word to the Words table 
 # check if string is a valid word
-# return error if cannot add to words
+# raise error if cannot add to words
 def words_set(ss = None):
     if forbiden(ss):
-        return False
-    # todo: add handeling of a False return
+        raise ForbidenError
     if not cpi.Words.Set(ss):
-        return False
+        raise Words.WordsError
     return True
 
 # get a word object from the Words table
 # check if string is a valid word
-# return error if cannot get word
+# raise error if cannot get word
 def words_get(ss = None):
-    # todo: add handling of false return
     if forbiden(ss):
-        return False
+        raise ForbidenError
     ret = cpi.Words.Get(ss)
     if not ret:
-        return False
-    wordobj = []
-    for item in ret:
-        wordobj.append(str(item))
-    return wordobj
+        raise Words.WordsError
+    return wordobj(ret)
+
+# get most recent word added to Words table 
+# raise error if connot get word
+def words_recent():
+    ret = cpi.Words.Recent()
+    if not ret:
+        raise Words.WordsError
+    return wordobj(ret)
+
+# get a list of new words from Words table
+# raise error if cannot get word
+def words_novellist(size = None):
+    ret = cpi.Words.Novel()
+    if not ret:
+        raise Words.WordsError
+    return formatwordlist(ret)
+
+# get a list of old words from Words table
+# raise error if cannot get word
+def words_oldlist():
+    ret = cpi.Words.Old(size = None):
+    if not ret:
+        raise Words.WordsError
+    return formatwordlist(ret)
+
+#############################################
+##      Tests                              ##
+#############################################
+
 #
 # unit testing for words functions
 def unit_test_words(wrd):
-    print("test words table functions:")
-    print("set the word:"+wrd)
-    ret = words_set(wrd)
-    print("returned:",end=' ')
-    print(ret)
-    print("get the word:"+wrd)
-    ret = words_get(wrd)
-    print("returned:",end=' ')
-    print(ret)
+    try:
+        print("test words table functions:")
+        print("set the word:"+wrd)
+        ret = words_set(wrd)
+        print("returned:",end='')
+        print(ret)
+        print("get the word:"+wrd)
+        ret = words_get(wrd)
+        print("returned:",end='')
+        print(ret)
+        print("get recent word:")
+        ret = words_recent()
+        print("returned:",end='')
+        print(ret)
+        print("get novel word list:")
+        ret = words_novellist()
+        print("returned:",end='')
+        print(ret)
+        print("get old word list:")
+        ret = words_oldlist()
+        print("returned:",end='')
+        print(ret)
+    except (Exception, Words.WordsError, ForbidenError) as error:
+        logging.basicConfig(filename='unit_test_words.log',logging.DEBUG)
+        logging.debug(error)
 
-
+###########################################
+# run tests 
 if __name__ == "__main__":
     print("API Testing...")
     unit_test_words('word')
--- CindAPI/Words.py
+++ CindAPI/Words.py
@@ -5,6 +5,11 @@
 import logging 
 import psycopg2 as psy
 
+# define error type WordsError
+# thrown by encapusulation of Words.py
+class WordsError(Exception):
+    pass
+
 # set a word to the Words table in the Mind
 # return false if err
 def Set(wrd = None):
@@ -12,7 +17,7 @@ def Set(wrd = None):
             return False
         qry = "select clock from words where lect=\'"+str(wrd)+"\';"
         add_new = "insert into words(lect,clock,new,old) values(\'"+str(wrd)+"\',CURRENT_TIMESTAMP,true,false) returning clock;"
-        add_old = "update words set clock=CURRENT_TIMESTAMP, new=true, old=false where lect=\'"+str(wrd)+"\';"
+        add_old = "update words set clock=CURRENT_TIMESTAMP where lect=\'"+str(wrd)+"\';"
         con = None
         try: 
             con = psy.connect(
@@ -37,30 +42,79 @@ def Set(wrd = None):
             if con is not None:
                 con.close()
 
+# qry and return responce
+# size allows limit of responce size
+# stop rewriting same code
+# DO NOT: update data or insert data
+def dbq(qry = None, size = None)
+    if qry is None:
+        return None
+    con = None
+    try:
+        con = psy.connect(
+            host = "localhost",
+            database = "mind",
+            user = "postgres",
+            password = "dirty")
+        cur = con.cursor()
+        cur.execute(qry)
+        if size is None:
+            return cur.fetchone()
+        ret = []
+        ret.append(cur.fetchall())
+        if len(ret) > size:
+            return ret[0:size]
+        else:
+            return ret
+    except (Exception, psy.DatabaseError) as error:
+        logging.basicConfig(filename='cindapierror.log',level=logging.DEBUG)
+        logging.debug(error)
+    finally:
+        if con is not None:
+            con.close
+
+
 # get word info from Words table in the Mind
 # return false if err
 def Get(wrd = None):
         if wrd is None:
                 return False
         qry = "select * from words where lect=\'"+str(wrd)+"\';"
-        con = None
-        try:
-            con = psy.connect(
-                    host = "localhost",
-                    database = "mind",
-                    user = "postgres",
-                    password = "dirty")
-            cur = con.cursor()
-            cur.execute(qry)
-            check = cur.fetchone()
-            if check is None:
-                    return False
-            return check
-        except (Exception, psy.DatabaseError) as error:
-            logging.basicConfig(filename='cindapierror.log',level=logging.DEBUG)
-            logging.debug(error)
-        finally:
-            if con is not None:
-                con.close()
+        check = dbq(qry)
+        if check is None:
+                return False
+        return check
 
+# get the most recent word info from Words table in the Mind
+# return false if err
+def Recent():
+    # todo: add a time variable to recal a specific time
+    # todo: time should be aproximate
+    qry = "select * from words where clock=(select MAX(clock) from words);"
+    check = dbq(qry)
+    if check is None:
+        return False
+    return check
+
+# get all new words from Words table in Mind
+# most recently used words first
+# return false if err
+def Novel():
+    # todo: decide how many to return, 10 for now
+    qry = "select * from words where new=True order by clock;"
+    check = dbq(qry,10)
+    if check is None:
+        return False
+    return check
+
+# get all old words
+# most recently used words first
+# return false if err
+def Old():
+    # todo: decide how many to return, 10 for now
+    qry = "select * from words where old=True order by clock;"
+    check = dbq(qry,10)
+    if check is None
+        return False
+    return check
 #EOF
