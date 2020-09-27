6646be35a7ef03628ab64bec84026da184032d11
nkevy <yvek.com@gmail.com>
2020-09-27 03:17:48 +0000

    bottle used for server, routes added

--- Server.py
+++ Server.py
@@ -1,40 +1,70 @@
+from bottle import route, run, request
+import Call as call
 import logging
-import socket 
-
-BUFFER_SIZE = 1
-
-# a server listenting to a port
-# takes json input from port
-class CindServer():
-    def __init__(self,host:str,port:int):
-        self.soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = host
-        self.port = port
-
-    def trigger(self,msg:str):
-        pass
-
-    def start(self):
-        with (self.soc) as s:
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            s.bind(((self.host,self.port))
-            s.listen()
-            while True:
-                conn, addr = s.accept()
-                msgdata = []
-                while True:
-                    buff = conn.recv(BUFFER_SIZE)
-                    buff.decode()
-                    if buff == ";":
-                        conn.close()
-                        trigger(msgdata)
-                    elif buff:
-                        msgdata.append(buff)
-                    else:
-                        break
+import sys
+## define routes for each method in the Cind API
 
 
-## todo: switch to bottlepy
+## route for set word to words table
+@route('/setword',method='POST')
+def words_set_route():
+    return call.words_set(request.forms.get('word'))
+
+## route for get word from words table
+@route('/getword',method='POST')
+def words_get_route():
+    return call.words_get(request.forms.get('word'))
+
+## route for get recent word from words table
+@route('/recentword')
+def words_recent_route():
+    return call.words_recent()
+
+## route for get list of novel words from words table
+@ route('/novelword',method='POST')
+def words_novel_route():
+    temp = request.forms.get('size')
+    if isinstance(temp,int):
+        return call.words_novel(request.forms.get('size'))
+    else:
+        return ({'Error':'Please send an integer'})
+
+## route for get list of old words from words table
+@ route('/oldword',method='POST')
+def words_old_route():
+    return call.words_old(request.forms.get('size'))
+
+## route for set a memory to memory table
+@ route('setmemory', method='POST')
+    return call.memory_set(
+            request.forms.get('word1'),
+            request.forms.get('word2'),
+            request.forms.get('decrement'))
+
+## route for get a memory from memory table
+@ route('getmemory',method='POST')
+    return call.memory_set(
+            request.forms.get('word1'),
+            request.forms.get('word2'))
+
+## route for get a list of memories from memory table
+@ route('getlistmemory',method='POST')
+    return call.memory_getlist(
+            request.forms.get('word1'),
+            requests.forms.get('size'))
+
+## route for get a sleep list, request a sleep
+@ route('/sleep',method='POST')
+    return call.sleep_list(request.forms.get('size'))
+
+# todo: write routes for all api calls
 if __name__ == '__main__':
-    csr = CindServer('127.0.0.1',8000)
-    csr.start()
+    if len(sys.argv)>1:
+        try:
+            run(host='localhost', port=int(sys.argv[1]), debug=True, reloader=True)
+        except (Exception) as error:
+            logging.basicConfig(filename='servererror.log',level=logging.DEBUG)
+            logging.debug(error)
+        exit(0)
+    else:
+        print('[USAGE] $python3 Server.py <Port number as argument>')
